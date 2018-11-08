2fca606946f323f104e5c58df91c6ed0710317b8
Fantosism <businessspamfolder@gmail.com>
2018-11-08 13:10:37 -0700

    barebones API module completed

--- scripts/Api.js
+++ scripts/Api.js
@@ -1,13 +1,56 @@
 /* eslint-env jquery */
 'use strict';
 
-const Api = function() {
+const Api = (function() {
   const BASE_URL = 'http://thinkful-list-api.herokuapp.com/bryan/bookmarks';
-  const getItems = function(callback) {
+  function getBookmark(callback) {
     $.getJSON(BASE_URL, callback);
 
   };
-  return {
-    getItems,
+
+  function createBookmark(name, url, description, rating, callback, errorCallback) {
+    const newBookmark = JSON.stringify(
+      {
+        name,
+        url, 
+        description,
+        rating,
+      }
+    );
+    $.ajax({
+      url: `${BASE_URL}`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newBookmark,
+      success: callback,
+      error: errorCallback,
+    });
   };
-};
+
+  function updateBookmark(id, updateData, callback, errorCallback) {
+    $.ajax({
+      url: `${BASE_URL}/${id}`,
+      method: 'PATCH',
+      contentType: 'application/json',
+      data: JSON.stringify(updateData),
+      success: callback,
+      error: errorCallback,
+    });
+  };
+
+  function deleteBookmark(id, updateData, callback, errorCallback) {
+    $.ajax({
+      url: `${BASE_URL}/${id}`,
+      method: 'DELETE',
+      success: callback,
+      error: errorCallback,
+    })
+  };
+
+  return {
+    getBookmark,
+    createBookmark,
+    updateBookmark,
+    deleteBookmark,
+  };
+})();
